910f9efc36c2be6ba5b6e8282f418c178209c581
Lincoln Thurlow <lincoln@isi.edu>
2020-06-06 11:07:45 -0700

    plumb grpcMaxCallMsgSize to grpcproxy server
    Signed-off-by: Lincoln Thurlow <lincoln@isi.edu>

--- etcdmain/grpc_proxy.go
+++ etcdmain/grpc_proxy.go
@@ -362,6 +362,8 @@ func newGRPCProxyServer(lg *zap.Logger, client *clientv3.Client) *grpc.Server {
 		grpc.StreamInterceptor(grpc_prometheus.StreamServerInterceptor),
 		grpc.UnaryInterceptor(grpc_prometheus.UnaryServerInterceptor),
 		grpc.MaxConcurrentStreams(math.MaxUint32),
+		grpc.MaxCallRecvMsgSize(grpcMaxCallRecvMsgSize),
+		grpc.MaxCallSendMsgSize(grpcMaxCallSendMsgSize),
 	)
 
 	pb.RegisterKVServer(server, kvp)
